e7a1b154e09ec26bc8e917498710b5fa099eac39
Ewan <2025797M@student.gla.ac.uk>
2015-10-19 20:56:53 +0100

    Updated some building info for building Debug & Release

--- E.Deezer/Properties/AssemblyInfo.cs
+++ E.Deezer/Properties/AssemblyInfo.cs
@@ -33,6 +33,6 @@ using System.Resources;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.0.7")]
-[assembly: AssemblyFileVersion("0.7.15292.1934")]
+[assembly: AssemblyVersion("0.0.0.8")]
+[assembly: AssemblyFileVersion("0.8.15292.1947")]
 [assembly: NeutralResourcesLanguageAttribute("en-GB")]
