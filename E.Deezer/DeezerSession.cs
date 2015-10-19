83535ee2220aa6076613b10a872659b7badff512
Ewan <2025797M@student.gla.ac.uk>
2015-10-19 21:16:38 +0100

    Added Permission Enum for authenticating users

---
+++ E.Deezer/DeezerSession.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace E.Deezer
+{
+    public class DeezerSession
+    {
+        public string Username { get; set; }
+        public string OAuthToken { get; set; }
+
+
+    }
+}
