0f2a9795aeca0f41c98271df9a35533c57724354
Ewan <2025797M@student.gla.ac.uk>
2015-10-24 19:00:23 +0100

    Updated user interface documentation

--- E.Deezer/Api/User.cs
+++ E.Deezer/Api/User.cs
@@ -5,23 +5,89 @@ using System.Text;
 
 namespace E.Deezer.Api
 {
+    /// <summary>
+    /// Deezer user object
+    /// </summary>
     public interface IUser
     {
+        /// <summary>
+        /// Deezer library ID number
+        /// </summary>
         uint Id { get; set; }
+
+        /// <summary>
+        /// Username
+        /// </summary>
         string Name { get; set; }
+
+        /// <summary>
+        /// Deezer.com link to user
+        /// </summary>
         string Link { get; set; }
 
-        ISearchResult<IAlbum> GetFavouriteAlbums();
-        ISearchResult<IArtist> GetFavouriteArtists();
-        ISearchResult<ITrack> GetFavouriteTracks();
-        ISearchResult<ITrack> GetUserFlow();
-        ISearchResult<ITrack> GetListenHistory();
-        ISearchResult<IPlaylist> GetFavouritePlaylists();
+        //Methods
 
-        ISearchResult<IAlbum> GetRecommendedAlbums();
-        ISearchResult<IArtist> GetRecommendedArtists();
-        ISearchResult<ITrack> GetRecommendedTracks();
-        ISearchResult<IPlaylist> GetRecommendedPlaylists();
+
+        /// <summary>
+        /// Gets the user's favourite albums
+        /// </summary>
+        /// <returns>First page of user's favourite albums</returns>
+        IPagedResponse<IAlbum> GetFavouriteAlbums();
+
+        /// <summary>
+        /// Gets the user's favourite artists
+        /// </summary>
+        /// <returns>First page of user's favourite artists</returns>
+        IPagedResponse<IArtist> GetFavouriteArtists();
+
+        /// <summary>
+        /// Gets the user's favourite tracks
+        /// </summary>
+        /// <returns>First page of user's favourite tracks</returns>
+        IPagedResponse<ITrack> GetFavouriteTracks();
+
+        /// <summary>
+        /// Gets the user's "flow"
+        /// </summary>
+        /// <returns>First page of user's "flow"</returns>
+        IPagedResponse<ITrack> GetUserFlow();
+
+        /// <summary>
+        /// Gets the user;s listening history
+        /// </summary>
+        /// <returns>First page of user's litening history</returns>
+        IPagedResponse<ITrack> GetListenHistory();
+        
+        /// <summary>
+        /// Gets the user's favourite playlists
+        /// </summary>
+        /// <returns>First page of user's listening history</returns>
+        IPagedResponse<IPlaylist> GetFavouritePlaylists();
+
+
+        /// <summary>
+        /// Gets recommended albums for the user
+        /// </summary>
+        /// <returns>First page of recommended albums</returns>
+        IPagedResponse<IAlbum> GetRecommendedAlbums();
+
+        /// <summary>
+        /// Gets recommended artists for the user
+        /// </summary>
+        /// <returns>First page of recommended artists</returns>
+        IPagedResponse<IArtist> GetRecommendedArtists();
+
+        /// <summary>
+        /// Gets recommended tracks for the user
+        /// </summary>
+        /// <returns>First page of recommended tracks</returns>
+        IPagedResponse<ITrack> GetRecommendedTracks();
+
+        /// <summary>
+        /// Gets recommended playlists for the user
+        /// </summary>
+        /// <returns>First page of recommded playlists</returns>
+        IPagedResponse<IPlaylist> GetRecommendedPlaylists();
 
     }
 
@@ -34,32 +100,32 @@ namespace E.Deezer.Api
         public string error { get; set; }
 
 
-        public ISearchResult<IAlbum> GetFavouriteAlbums()
+        public IPagedResponse<IAlbum> GetFavouriteAlbums()
         {
             return null;
         }
 
-        public ISearchResult<IArtist> GetFavouriteArtists()
+        public IPagedResponse<IArtist> GetFavouriteArtists()
         {
             return null;
         }
 
-        public ISearchResult<ITrack> GetFavouriteTracks()
+        public IPagedResponse<ITrack> GetFavouriteTracks()
         {
             return null;
         }
 
-        public ISearchResult<ITrack> GetUserFlow()
+        public IPagedResponse<ITrack> GetUserFlow()
         {
             return null;
         }
 
-        public ISearchResult<ITrack> GetListenHistory()
+        public IPagedResponse<ITrack> GetListenHistory()
         {
             return null;
         }
 
-        public ISearchResult<IPlaylist> GetFavouritePlaylists()
+        public IPagedResponse<IPlaylist> GetFavouritePlaylists()
         {
             return null;
         }
@@ -67,22 +133,22 @@ namespace E.Deezer.Api
 
 
 
-        public ISearchResult<IAlbum> GetRecommendedAlbums()
+        public IPagedResponse<IAlbum> GetRecommendedAlbums()
         {
             return null;
         }
 
-        public ISearchResult<IArtist> GetRecommendedArtists()
+        public IPagedResponse<IArtist> GetRecommendedArtists()
         {
             return null;
         }
 
-        public ISearchResult<ITrack> GetRecommendedTracks()
+        public IPagedResponse<ITrack> GetRecommendedTracks()
         {
             return null;
         }
 
-        public ISearchResult<IPlaylist> GetRecommendedPlaylists()
+        public IPagedResponse<IPlaylist> GetRecommendedPlaylists()
         {
             return null;
         }
