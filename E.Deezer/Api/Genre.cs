1804684e3031e1a1c674c763961db76e339c0cea
Ewan <2025797M@student.gla.ac.uk>
2015-11-19 20:50:31 +0000

    Added basic genre properties

--- E.Deezer/Api/Genre.cs
+++ E.Deezer/Api/Genre.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
+using RestSharp.Deserializers;
+
 namespace E.Deezer.Api
 {
     /// <summary>
@@ -20,17 +22,84 @@ namespace E.Deezer.Api
         /// </summary>
         string Name { get; set; }
 
+        //METHODS
+
         /// <summary>
-        /// Link to genre's image
+        /// Gets Genre's image
         /// </summary>
-        string Picture { get; set; }
+        /// <param name="aSize">Requested image size</param>
+        /// <returns>Url to image, or empty string if no such image exists.</returns>
+        string GetPicture(PictureSize aSize);
+
+        /// <summary>
+        /// Gets if the Genre has the specified image
+        /// </summary>
+        /// <param name="aSize">Requested image size</param>
+        /// <returns>True if picture exists</returns>
+        bool HasPicture(PictureSize aSize);
     }
+
+    /// <summary>
+    /// Defines a picture size for Deezer Genre
+    /// </summary>
+    public enum PictureSize
+    {
+        /// <summary>
+        /// Small picture size -> 56x56 pixels
+        /// </summary>
+        SMALL,
+ 
+        /// <summary>
+        /// Medium picture size -> 250x250 pixels
+        /// </summary>
+        MEDIUM,
+        
+        /// <summary>
+        /// Large picture size -> 500x500 pixels
+        /// </summary>
+        LARGE,
+    };
+
 
     internal class Genre : IGenre
     {
         public uint Id { get; set; }
         public string Name { get; set; }
-        public string Picture { get; set; }
+
+        //Pictures
+        [DeserializeAs(Name="picture_small")]
+        private string SMPicture { get; set; }
+
+        [DeserializeAs(Name = "picture_medium")]
+        private string MDPicture { get; set; }
+
+        [DeserializeAs(Name = "picture_big")]
+        private string BGPicture { get; set; }
+
+
+        //Methods
+
+        public string GetPicture(PictureSize aSize)
+        {
+            switch(aSize)
+            {
+                case PictureSize.SMALL: { return string.IsNullOrEmpty(SMPicture) ? string.Empty : SMPicture; }
+                case PictureSize.MEDIUM: { return string.IsNullOrEmpty(MDPicture) ? string.Empty : MDPicture; }
+                case PictureSize.LARGE: { return string.IsNullOrEmpty(BGPicture) ? string.Empty : BGPicture; }
+                default: { return string.Empty; }
+            }
+        }
+
+        public bool HasPicture(PictureSize aSize)
+        {
+            switch (aSize)
+            {
+                case PictureSize.SMALL: { return string.IsNullOrEmpty(SMPicture); }
+                case PictureSize.MEDIUM: { return string.IsNullOrEmpty(MDPicture); }
+                case PictureSize.LARGE: { return string.IsNullOrEmpty(BGPicture); }
+                default: { return false; }
+            }
+        }
 
 
         public override string ToString()
