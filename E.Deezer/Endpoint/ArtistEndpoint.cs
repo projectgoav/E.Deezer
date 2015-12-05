892cb799dce3223c4eaebcbe3f55a420624dba8a
Ewan <2025797M@student.gla.ac.uk>
2015-12-05 17:37:17 +0000

    Added V2 Endpoints

---
+++ E.Deezer/Endpoint/ArtistEndpoint.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace E.Deezer.Endpoint
+{
+    public interface IArtistEndpoint
+    {
+
+    }
+
+    internal class ArtistEndpoint : IArtistEndpoint
+    {
+    }
+}
