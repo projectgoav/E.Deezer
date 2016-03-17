93638f522acf9d24252985b04ed176e1f475dc00
Ewan <2025797M@student.gla.ac.uk>
2016-03-17 21:02:44 +0000

    Implemented a ExecutorService to push the API request out of the client which is doing error processing, authenitcation and making requestes

---
+++ E.Deezer/ExecutorService.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Threading;
+using System.Threading.Tasks;
+
+using RestSharp;
+
+namespace E.Deezer
+{
+    internal class ExecutorService
+    {
+        private const int DEFAULT_TIMEOUT = 2500;
+
+        private readonly RestClient iClient;
+        private readonly CancellationTokenSource iCancellationTokenSource;
+
+        internal ExecutorService()
+        {
+            iCancellationTokenSource = new CancellationTokenSource();
+
+            iClient = new RestClient(DeezerSession.ENDPOINT);
+            iClient.Timeout = DEFAULT_TIMEOUT;
+        }
+
+        internal CancellationToken CancellationToken { get { return iCancellationTokenSource.Token; } }
+
+
+        public Task<IRestResponse<T>> ExecuteGet<T>(string aMethod, IEnumerable<IRequestParameter> aParams)
+        {
+            return Execute<T>(aMethod, Method.POST, aParams);
+        }
+
+        public Task<IRestResponse<T>> ExecutePost<T>(string aMethod, IEnumerable<IRequestParameter> aParams)
+        {
+            return Execute<T>(aMethod, Method.POST, aParams);
+        }
+
+
+
+        private Task<IRestResponse<T>> Execute<T>(string aMethodUrl, Method aMethodType, IEnumerable<IRequestParameter> aParams)
+        {
+            IRestRequest request = new RestRequest(aMethodUrl, aMethodType);
+            AddParamsToRequest(request, aParams);
+
+            return iClient.ExecuteTaskAsync<T>(request, CancellationToken);
+        }
+
+        private void AddParamsToRequest(IRestRequest aRequest, IEnumerable<IRequestParameter> aParams)
+        {
+            foreach(IRequestParameter param in aParams)
+            {
+                aRequest.AddParameter(param.Id, param.Value, param.Type);
+            }
+
+            //Always add the correct output format
+            aRequest.AddParameter("output", "json", ParameterType.QueryString);
+        }
+
+
+    }
+}
---
+++ E.Deezer/RequestParameter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using RestSharp;
+
+namespace E.Deezer
+{
+    internal interface IRequestParameter
+    {
+        ParameterType Type { get; }
+        string Id          { get; }
+        string Value       { get; }
+    }
+
+
+    internal class RequestParameter
+    {
+        private ParameterType iType;
+        private string iId;
+        private string iValue;
+
+        public RequestParameter(string aId, string aValue, ParameterType aType)
+        {
+            iType = aType;
+            iId = aId;
+            iValue = aValue;
+        }
+
+        public ParameterType Type { get { return iType; } }
+        public string Id          { get { return iId; } }
+        public string Value       { get { return iValue; } }
+    }
+}
