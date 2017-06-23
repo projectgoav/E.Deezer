431117601a07afd308b334839a503f08136eb8e6
Xavier Fischer <xfischer@users.noreply.github.com>
2017-06-23 15:18:40 +0200

    Create and add songs to playlists (#41)

---
+++ E.Deezer/Api/Internal/DeezerCreateResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using RestSharp.Deserializers;
+
+namespace E.Deezer.Api
+{
+    //Retrun value of all Deezer API calls
+    internal class DeezerCreateResponse
+    {
+        [DeserializeAs(Name = "id")]
+        public uint Id { get; set; }
+    }
+}
