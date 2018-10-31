5042931263fc9104c81c8ed3033d8848fd239430
Tolbxela <tolbxela@gmail.com>
2018-11-01 00:00:31 +0100

    SearchEndpoint was extended with Search Advanced method

--- E.Deezer/Endpoint/SearchEndpoint.cs
+++ E.Deezer/Endpoint/SearchEndpoint.cs
@@ -1,30 +1,30 @@
-﻿using System;
+﻿using E.Deezer.Api;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
 using System.Threading.Tasks;
-
-using E.Deezer.Api;
 
 namespace E.Deezer.Endpoint
 {
-    public interface  ISearchEndpoint
+    public interface ISearchEndpoint
     {
         Task<IEnumerable<IAlbum>> Albums(string aQuery, uint aStart = 0, uint aCount = 100);
 
         Task<IEnumerable<IArtist>> Artists(string aQuery, uint aStart = 0, uint aCount = 100);
 
-        Task<IEnumerable<IPlaylist>> Playlists(string aQuery, uint aStart = 0 , uint aCount = 100);
+        Task<IEnumerable<IPlaylist>> Playlists(string aQuery, uint aStart = 0, uint aCount = 100);
 
         Task<IEnumerable<ITrack>> Tracks(string aQuery, uint aStart = 0, uint aCount = 100);
 
-        Task<IEnumerable<IRadio>> Radio(string aQuery, uint aStart  = 0, uint aCount = 100);
+        Task<IEnumerable<IRadio>> Radio(string aQuery, uint aStart = 0, uint aCount = 100);
+
+        Task<IEnumerable<IUser>> User(string aQuery, uint aStart = 0, uint aCount = 100);
+
+        Task<IEnumerable<ITrack>> Advanced(string aQuery, uint aStart = 0, uint aCount = 100);
+
     }
 
     internal class SearchEndpoint : ISearchEndpoint
     {
-        private const string SEARCH_BASE = "search/";
+        private const string SEARCH_BASE = "search";
 
         private readonly DeezerClient iClient;
 
@@ -34,25 +34,32 @@ namespace E.Deezer.Endpoint
         }
 
 
-        public Task<IEnumerable<IAlbum>> Albums(string aQuery, uint aStart = 0, uint aCount = 100) 
-            => Get<Album, IAlbum>("album", aQuery, aStart, aCount); 
+        public Task<IEnumerable<IAlbum>> Albums(string aQuery, uint aStart = 0, uint aCount = 100)
+            => Get<Album, IAlbum>("album", aQuery, aStart, aCount);
 
-        public Task<IEnumerable<IArtist>> Artists(string aQuery, uint aStart = 0, uint aCount = 100) 
-            => Get<Artist, IArtist>("artist", aQuery, aStart, aCount); 
+        public Task<IEnumerable<IArtist>> Artists(string aQuery, uint aStart = 0, uint aCount = 100)
+            => Get<Artist, IArtist>("artist", aQuery, aStart, aCount);
 
-        public Task<IEnumerable<IPlaylist>> Playlists(string aQuery, uint aStart = 0, uint aCount = 100) 
-            => Get<Playlist, IPlaylist>("playlist", aQuery, aStart, aCount); 
+        public Task<IEnumerable<IPlaylist>> Playlists(string aQuery, uint aStart = 0, uint aCount = 100)
+            => Get<Playlist, IPlaylist>("playlist", aQuery, aStart, aCount);
 
-        public Task<IEnumerable<ITrack>> Tracks(string aQuery, uint aStart = 0, uint aCount = 100) 
-            => Get<Track, ITrack>("track", aQuery, aStart, aCount);   
+        public Task<IEnumerable<ITrack>> Tracks(string aQuery, uint aStart = 0, uint aCount = 100)
+            => Get<Track, ITrack>("track", aQuery, aStart, aCount);
 
-        public Task<IEnumerable<IRadio>> Radio(string aQuery, uint aStart = 0, uint aCount = 100) 
-            => Get<Radio, IRadio>("radio", aQuery, aStart, aCount); 
+        public Task<IEnumerable<IRadio>> Radio(string aQuery, uint aStart = 0, uint aCount = 100)
+            => Get<Radio, IRadio>("radio", aQuery, aStart, aCount);
 
+        public Task<IEnumerable<IUser>> User(string aQuery, uint aStart = 0, uint aCount = 100)
+            => Get<User, IUser>("user", aQuery, aStart, aCount);
+
+        public Task<IEnumerable<ITrack>> Advanced(string aQuery, uint aStart = 0, uint aCount = 100)
+            => Get<Track, ITrack>(string.Empty, aQuery, aStart, aCount);
 
         private Task<IEnumerable<TDest>> Get<TSource, TDest>(string aSearchEndpoint, string aQuery, uint aStart, uint aCount) where TSource : TDest, IDeserializable<IDeezerClient>
         {
-            string method = string.Format("{0}{1}", SEARCH_BASE, aSearchEndpoint);
+            string method = (aSearchEndpoint.Length == 0) ? 
+                    SEARCH_BASE : 
+                    string.Format("{0}/{1}", SEARCH_BASE, aSearchEndpoint);
 
             List<IRequestParameter> parms = new List<IRequestParameter>()
             {
@@ -60,10 +67,10 @@ namespace E.Deezer.Endpoint
             };
 
             return iClient.Get<TSource>(method, parms, aStart, aCount)
-                          .ContinueWith<IEnumerable<TDest>>((aTask) =>
-                            {
-                                return iClient.Transform<TSource, TDest>(aTask.Result);
-                            }, iClient.CancellationToken, TaskContinuationOptions.NotOnCanceled, TaskScheduler.Default);
+                .ContinueWith<IEnumerable<TDest>>((aTask) =>
+                {
+                    return iClient.Transform<TSource, TDest>(aTask.Result);
+                }, iClient.CancellationToken, TaskContinuationOptions.NotOnCanceled, TaskScheduler.Default);
         }
 
     }
