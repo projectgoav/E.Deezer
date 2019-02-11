2ef169dfb9ccc18b1e51698a6fe0919e50d4e779
Ewan <git@ewan.emccartney.co.uk>
2019-02-11 18:40:34 +0000

    Created a multi-targetting solution to target mutiple different frameworks.

--- E.Deezer/Properties/AssemblyInfo.cs
+++ E.Deezer/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.2.1.524")]
-[assembly: AssemblyFileVersion("3.2.1.524")]
+[assembly: AssemblyVersion("3.2.1.528")]
+[assembly: AssemblyFileVersion("3.1902.11.528")]
