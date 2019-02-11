16759400e359efca105881578f56a61eca2fbdc8
Ewan <git@ewan.emccartney.co.uk>
2019-02-11 19:11:43 +0000

    Update the E.Deezer project to build package automagically on build.

--- E.Deezer/Properties/AssemblyInfo.cs
+++ E.Deezer/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.2.1.528")]
-[assembly: AssemblyFileVersion("3.1902.11.528")]
+[assembly: AssemblyVersion("4.0.0.541")]
+[assembly: AssemblyFileVersion("4.0.0.0")]
