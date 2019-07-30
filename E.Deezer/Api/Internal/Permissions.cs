c67b224fbcaeab86feea19f2285e1fd084a4e2df
Ewan <git@ewan.emccartney.co.uk>
2019-07-30 19:47:42 +0100

    Remove uses of HasFlag.

--- E.Deezer/Api/Internal/Permissions.cs
+++ E.Deezer/Api/Internal/Permissions.cs
@@ -47,40 +47,27 @@ namespace E.Deezer.Api
         {
             bool permission = true;
 
-            if (aPermission.HasFlag(DeezerPermissions.BasicAccess))
-            {
+            if (aPermission.HasPermission(DeezerPermissions.BasicAccess))
                 permission &= HasBasicAccess;
-            }
 
-            if (aPermission.HasFlag(DeezerPermissions.DeleteLibrary))
-            {
+            if (aPermission.HasPermission(DeezerPermissions.DeleteLibrary))
                 permission &= HasDeleteLibrary;
-            }
 
-            if (aPermission.HasFlag(DeezerPermissions.Email))
-            {
+            if (aPermission.HasPermission(DeezerPermissions.Email))
                 permission &= HasEmail; 
-            }
 
-            if (aPermission.HasFlag(DeezerPermissions.ListeningHistory))
-            {
+            if (aPermission.HasPermission(DeezerPermissions.ListeningHistory))
                 permission &= HasListeningHistory; 
-            }
 
-            if (aPermission.HasFlag(DeezerPermissions.ManageCommunity))
-            {
+            if (aPermission.HasPermission(DeezerPermissions.ManageCommunity))
                 permission &= HasManageCommunity; 
-            }
 
-            if (aPermission.HasFlag(DeezerPermissions.ManageLibrary))
-            {
+            if (aPermission.HasPermission(DeezerPermissions.ManageLibrary))
                 permission &= HasManageLibrary;
-            }
 
-            if (aPermission.HasFlag(DeezerPermissions.OfflineAccess))
-            {
+            if (aPermission.HasPermission(DeezerPermissions.OfflineAccess))
                 permission &= HasOfflineAccess;
-            }
+
 
             return permission;
         }
