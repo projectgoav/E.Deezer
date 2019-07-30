5e6107027458651ebfdd278da9e2957937386ce2
Ewan <git@ewan.emccartney.co.uk>
2019-07-30 19:31:24 +0100

    Some changes to permissions

--- E.Deezer/DeezerPermissions.cs
+++ E.Deezer/DeezerPermissions.cs
@@ -7,56 +7,45 @@ namespace E.Deezer
 {
     public static class Permissions
     {
-        private const string dBasicAccess = "basic_access";
-        private const string dEmail = "email";
-        private const string dOffliceAccess = "offline_access";
-        private const string dManageLibrary = "manage_library";
-        private const string dManageCommunity = "manage_community";
-        private const string dDeleteLibrary = "delete_library";
-        private const string dListeningHistory = "listening_history";
+        internal const string BASIC_ACCESS      = "basic_access";
+        internal const string EMAIL             = "email";
+        internal const string OFFLINE_ACCESS    = "offline_access";
+        internal const string MANAGE_LIBRARY    = "manage_library";
+        internal const string MANAGE_COMMUNITY  = "manage_community";
+        internal const string DELETE_LIBRARY    = "delete_library";
+        internal const string LISTENING_HISTORY = "listening_history";
 
-
-        public static string PermissionToString(this DeezerPermissions aPermission)
+        public static string PermissionToString(this DeezerPermissions permissions)
         {
-            List<string> perms = new List<string>(10);
+            List<string> perms = new List<string>(8);
 
-            if (aPermission.HasFlag(DeezerPermissions.BasicAccess))
-            {
-                perms.Add(dBasicAccess);
-            }
+            if (permissions.HasPermission(DeezerPermissions.BasicAccess))
+                perms.Add(BASIC_ACCESS);
 
-            if (aPermission.HasFlag(DeezerPermissions.DeleteLibrary))
-            {
-                perms.Add(dDeleteLibrary);
-            }
+            if (permissions.HasPermission(DeezerPermissions.Email))
+                perms.Add(EMAIL);
 
-            if (aPermission.HasFlag(DeezerPermissions.Email))
-            {
-                perms.Add(dEmail);
-            }
+            if (permissions.HasPermission(DeezerPermissions.DeleteLibrary))
+                perms.Add(DELETE_LIBRARY);
 
-            if (aPermission.HasFlag(DeezerPermissions.ListeningHistory))
-            {
-                perms.Add(dListeningHistory);
-            }
+            if (permissions.HasPermission(DeezerPermissions.ListeningHistory))
+                perms.Add(LISTENING_HISTORY);
 
-            if (aPermission.HasFlag(DeezerPermissions.ManageCommunity))
-            {
-                perms.Add(dManageCommunity);
-            }
+            if (permissions.HasPermission(DeezerPermissions.ManageCommunity))
+                perms.Add(MANAGE_COMMUNITY);
 
-            if (aPermission.HasFlag(DeezerPermissions.ManageLibrary))
-            {
-                perms.Add(dManageLibrary);
-            }
+            if (permissions.HasPermission(DeezerPermissions.ManageLibrary))
+                perms.Add(MANAGE_LIBRARY);
 
-            if (aPermission.HasFlag(DeezerPermissions.OfflineAccess))
-            {
-                perms.Add(dOffliceAccess);
-            }
+            if (permissions.HasPermission(DeezerPermissions.OfflineAccess))
+                perms.Add(OFFLINE_ACCESS);
 
             return string.Join(", ", perms);
         }
+
+
+        public static bool HasPermission(this DeezerPermissions permissions, DeezerPermissions expectedPermission)
+            => (permissions & expectedPermission) > 0;
     }
 
     /// <summary>
@@ -69,40 +58,40 @@ namespace E.Deezer
         /// Access users basic information.
         /// INFO: Include name, firstname, profile picture, gender
         /// </summary>
-        BasicAccess = 1,
+        BasicAccess = 1 << 0,
 
         /// <summary>
         /// Get the user's email
         /// </summary>
-        Email = 2,
+        Email = 1 << 1,
 
         /// <summary>
         /// Access user data any time
         /// NOTE: Almost the same as all other permissions
         /// </summary>
-        OfflineAccess = 4,
+        OfflineAccess = 1 << 2,
 
         /// <summary>
         /// Manage users' library 
         /// INFO: Add/rename a playlist. Add/order songs in the playlist.
         /// </summary>
-        ManageLibrary = 8,
+        ManageLibrary = 1 << 3,
 
         /// <summary>
         /// Manage users' friends
         /// INFO: Add/remove a following/follower.
         /// </summary>
-        ManageCommunity = 16,
+        ManageCommunity = 1 << 4,
 
         /// <summary>
         /// Delete library items 	
         /// INFO: Allow the application to delete items in the user's library
         /// </summary>
-        DeleteLibrary = 32,
+        DeleteLibrary = 1 << 5,
 
         /// <summary>
         /// Allow the application to access the user's listening history
         /// </summary>
-        ListeningHistory = 64,
+        ListeningHistory = 1 << 6,
     }
 }
