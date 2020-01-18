23ff61196da10badd1f7dd1ed7cab737622752b4
Ewan <git@ewan.emccartney.co.uk>
2020-01-18 22:01:48 +0000

    Uprev to v5 before testing automagic Nuget tag and upload.

--- E.Deezer/Properties/AssemblyInfo.cs
+++ E.Deezer/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("E")]
 [assembly: AssemblyProduct("E.Deezer.Portable")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("4.2.2.548")]
-[assembly: AssemblyFileVersion("4.2.2.0")]
+[assembly: AssemblyVersion("5.0.0.548")]
+[assembly: AssemblyFileVersion("5.0.0.0")]
