8913a8f9c1f0901e05b08d9e45c219388f44bf44
Ewan <git@ewan.emccartney.co.uk>
2020-02-18 20:31:23 +0000

    Fix stackoverflow in compat extensions.

--- E.Deezer/Util/CompatExtensions.cs
+++ E.Deezer/Util/CompatExtensions.cs
@@ -46,8 +46,8 @@ namespace E.Deezer.Util
             public static IEnumerable<T> Empty<T>()
             {
 #if NETSTANDARD20
-                return Array.Empty<T>();
-#else
+                return System.Array.Empty<T>();
+#else     
                 return new T[0];
 #endif
             }
